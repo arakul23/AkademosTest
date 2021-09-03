6135597bd02b7abc7d49a5f976c6a5303504e57e
arakul23 <babls2332@gmail.com>
2021-09-03 19:47:31 +0300

    Поправлена функция в task4.php

--- task4.php
+++ task4.php
@@ -4,10 +4,14 @@ function carry(array $arr) : int
 {
     $arr1 = str_split($arr[0]);
     $arr2 = str_split($arr[1]);
+    $arr1 = array_reverse($arr1);
+    $arr2 = array_reverse($arr2);
     $carrySum = 0;
 
     foreach ($arr1 as $key => $value) {
-        if ($arr1[$key] + $arr2[$key] >= 10) {
+        $digit1 = isset($arr1[$key]) ? $arr1[$key] : 0;
+        $digit2 = isset($arr2[$key]) ? $arr2[$key] : 0;
+        if ($digit1 + $digit2 >= 10) {
             $carrySum += 1;
         }
     }
@@ -15,6 +19,11 @@ function carry(array $arr) : int
     return $carrySum;
 }
 
-echo carry([123, 456]) . '<br>'; // 0
+echo carry([1254, 9999]) . '<br>'; // 4
 echo carry([555, 555]) . '<br>'; // 3
 echo carry([123, 594]) . '<br>'; // 1
+echo carry([123, 594]) . '<br>'; // 1
+echo carry([90, 1]) . '<br>'; // 0
+echo carry([0, 0]) . '<br>'; // 0
+echo carry([45, 55]) . '<br>'; // 1
+echo carry([1, 99]) . '<br>'; // 1
