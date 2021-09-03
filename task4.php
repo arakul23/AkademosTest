3af0bd3c2d550836985d02248163e0ebc86ea630
arakul23 <babls2332@gmail.com>
2021-09-04 00:19:19 +0300

    Переделана функция в task4.php

--- task4.php
+++ task4.php
@@ -2,28 +2,52 @@
 
 function carry(array $arr) : int
 {
+    $carry = 0;
+
+    $count = 0;
+
     $arr1 = str_split($arr[0]);
     $arr2 = str_split($arr[1]);
-    $arr1 = array_reverse($arr1);
-    $arr2 = array_reverse($arr2);
-    $carrySum = 0;
 
-    foreach ($arr1 as $key => $value) {
-        $digit1 = isset($arr1[$key]) ? $arr1[$key] : 0;
-        $digit2 = isset($arr2[$key]) ? $arr2[$key] : 0;
-        if ($digit1 + $digit2 >= 10) {
-            $carrySum += 1;
+    $arr1Count = count($arr1);
+    $arr2Count = count($arr2);
+
+    while ($arr1Count !== 0 || $arr2Count !== 0)
+    {
+        $x = 0;
+        $y = 0;
+
+        if ($arr1Count > 0)
+        {
+            $x = $arr1[$arr1Count - 1];
+            $arr1Count--;
         }
+
+        if ($arr2Count > 0)
+        {
+            $y = $arr2[$arr2Count - 1];
+            $arr2Count--;
+        }
+
+        $sum = $x + $y + $carry;
+
+        if ($sum >= 10)
+        {
+            $carry = 1;
+            $count++;
+        }
+
+        else
+            $carry = 0;
     }
 
-    return $carrySum;
+    return $count;
 }
 
 echo carry([1254, 9999]) . '<br>'; // 4
 echo carry([555, 555]) . '<br>'; // 3
 echo carry([123, 594]) . '<br>'; // 1
-echo carry([123, 594]) . '<br>'; // 1
 echo carry([90, 1]) . '<br>'; // 0
 echo carry([0, 0]) . '<br>'; // 0
-echo carry([45, 55]) . '<br>'; // 1
-echo carry([1, 99]) . '<br>'; // 1
+echo carry([45, 55]) . '<br>'; // 2
+echo carry([1, 99]) . '<br>'; // 2
